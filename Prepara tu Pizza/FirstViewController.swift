ab1874989be82888e25ddd064d6edecffa2930ab
Gerardo Valencia <Mcklopedia@gerardos-mac.local>
2015-12-11 20:28:35 -0800

    Initial Commit

---
+++ Prepara tu Pizza/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Prepara tu Pizza
+//
+//  Created by Gerardo Valencia on 12/11/15.
+//  Copyright © 2015 Gerardo Valencia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
